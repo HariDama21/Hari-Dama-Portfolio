d690f6987d08a169544f2ba8fdd2c0db7837673d
Hari Dama <106622827+HariDama21@users.noreply.github.com>
2024-01-29 22:25:52 +0530

    Update 1_👋🏼_Welcome.py

--- 1_👋🏼_Welcome.py
+++ 1_👋🏼_Welcome.py
@@ -41,10 +41,10 @@ if selected=="✨ Home":
     col1, col2 = st.columns(2)
     with col1:
         def load_lottie_url(url: str):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
+            r = requests.get(url)
+            if r.status_code != 200:
+                return None
+            return r.json()
 
     lottie_animation_1 = "https://lottie.host/9f38b8b5-3d51-4466-9e90-e57de7c47142/46DUUW0Z0Y.json"
     lottie_anime_json1 = load_lottie_url(lottie_animation_1)
