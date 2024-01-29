18b0f3c4db2ffa8e4fce0966a90d79155e06f062
Hari Dama <106622827+HariDama21@users.noreply.github.com>
2024-01-29 21:48:07 +0530

    Update 1_👋🏼_Welcome.py

--- 1_👋🏼_Welcome.py
+++ 1_👋🏼_Welcome.py
@@ -49,7 +49,7 @@ if selected=="✨ Home":
 
     lottie_animation_1 = "https://lottie.host/9f38b8b5-3d51-4466-9e90-e57de7c47142/46DUUW0Z0Y.json"
     lottie_anime_json = load_lottie_url(lottie_animation_1)
-    st_lottie(lottie_anime_json,  key="initial")
+    st_lottie(lottie_anime_json,  width=400, height=600, key="initial")
 
     st.title("Hari Dama")
 
